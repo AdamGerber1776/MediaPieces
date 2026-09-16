80432c08896bd154f8985c5eee3760181adf2875
agerber2 <gerber111904@gmail.com>
2026-09-16 14:31:35 -0500

    Randomized Piece Location and Created Puzzle Board
    I further developed puzzle placement and UI logic to place the puzzle pieces on the side of the board and created the puzzle board for the pieces to go into.

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -1,34 +1,127 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
+    [SerializeField] private Material cellMaterial;
+    [SerializeField] private Material gridMaterial;
+
+    public static PuzzleManager Instance;
+    
+    private void Awake()
+    {
+        //helps prevent the possibility of having two competing instances of this class
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
     public static void CreatePuzzleFromImage(Texture2D image)
     {
         Debug.Log("Creating puzzle from image of size: " + image.width + "x" + image.height);
 
         int difficulty = 3; //temporarily set difficulty value. to be determined by selection later
-        int pieceWidth = image.width / 10 / difficulty;
-        int pieceHeight = image.height / 10 / difficulty;
+        float pieceWidth = image.width / 10f / difficulty;
+        float pieceHeight = image.height / 10f / difficulty;
         float pieceWidthPercent = 1.0f / difficulty;
         float pieceHeightPercent = 1.0f / difficulty;
+        float boardWidth = pieceWidth * difficulty;
+        float boardHeight = pieceHeight * difficulty;
+        float gridGap = 0.2f; //gap between pieces of the board to create the grid
+
         int[] triangles =
         {
             0, 2, 1,
             2, 3, 1
         };
 
+        //Creates the board grid
+        GameObject boardGrid = new GameObject("PuzzleBoardGrid");
+        Mesh gridMesh = new Mesh();
+        Vector3[] gridVertices =
+        {
+            new Vector3(-boardWidth / 2f - gridGap / 2f, -boardHeight / 2f - gridGap / 2f, 0),
+            new Vector3( boardWidth / 2f + gridGap / 2f, -boardHeight / 2f - gridGap / 2f, 0),
+            new Vector3(-boardWidth / 2f - gridGap / 2f,  boardHeight / 2f + gridGap / 2f, 0),
+            new Vector3( boardWidth / 2f + gridGap / 2f,  boardHeight / 2f + gridGap / 2f, 0)
+        };
+
+        gridMesh.vertices = gridVertices;
+        gridMesh.triangles = triangles;
+
+        MeshFilter gridFilter = boardGrid.AddComponent<MeshFilter>();
+        MeshRenderer gridRenderer = boardGrid.AddComponent<MeshRenderer>();
+
+        gridFilter.mesh = gridMesh;
+        gridRenderer.material = PuzzleManager.Instance.gridMaterial;
+
+        boardGrid.transform.position = new Vector3(
+                    0,
+                    0,
+                    2
+                );
+
+        //creates the puzzle board
         for (int x = 0; x < difficulty; x++)
         {
             for (int y = 0; y < difficulty; y++)
             {
-                GameObject piece = new GameObject("PuzzlePiece");
+                GameObject boardPiece = new GameObject("PuzzleBoardPiece " + "x:" + x + " y:" + y);
                 Mesh mesh = new Mesh();
                 Vector3[] vertices =
                 {
-                    new Vector3(-pieceWidth, -pieceHeight, 0),
-                    new Vector3(pieceWidth, -pieceHeight, 0),
-                    new Vector3(-pieceWidth,  pieceHeight, 0),
-                    new Vector3(pieceWidth,  pieceHeight, 0)
+                    new Vector3(-pieceWidth / 2f + gridGap / 2f, -pieceHeight / 2f + gridGap / 2f, 0),
+                    new Vector3( pieceWidth / 2f - gridGap / 2f, -pieceHeight / 2f + gridGap / 2f, 0),
+                    new Vector3(-pieceWidth / 2f + gridGap / 2f,  pieceHeight / 2f - gridGap / 2f, 0),
+                    new Vector3( pieceWidth / 2f - gridGap / 2f,  pieceHeight / 2f - gridGap / 2f, 0)
+                };
+
+                mesh.vertices = vertices;
+                mesh.triangles = triangles;
+
+                MeshFilter filter = boardPiece.AddComponent<MeshFilter>();
+                MeshRenderer renderer = boardPiece.AddComponent<MeshRenderer>();
+
+                filter.mesh = mesh;
+                renderer.material = PuzzleManager.Instance.cellMaterial;
+
+                float posX = -boardWidth / 2f + pieceWidth / 2f + x * pieceWidth;
+                float posY = -boardHeight / 2f + pieceHeight / 2f + y * pieceHeight;
+
+                boardPiece.transform.position = new Vector3(
+                    posX,
+                    posY,
+                    1
+                );
+            }
+        }
+
+        //zooms the camera to fit the puzzle board
+        CameraHandler.Instance.FrameBoard(boardWidth, boardHeight);
+
+        //variables to determine piece random placement location
+        float boardLeft   = -boardWidth / 2f;
+        float boardRight  =  boardWidth / 2f;
+        float piecesPadding = 1f; //padding to prevent pieces from being placed too close to the board edges
+        float piecesSpace = pieceWidth * 2f; //size of the space in which the pieces can be placed on either side of the board
+
+        //creates the puzzle pieces and places them on the board
+        for (int x = 0; x < difficulty; x++)
+        {
+            for (int y = 0; y < difficulty; y++)
+            {
+                GameObject piece = new GameObject("PuzzlePiece " + "x:" + x + " y:" + y);
+                Mesh mesh = new Mesh();
+                Vector3[] vertices =
+                {
+                    new Vector3(-pieceWidth / 2f, -pieceHeight / 2f, 0),
+                    new Vector3( pieceWidth / 2f, -pieceHeight / 2f, 0),
+                    new Vector3(-pieceWidth / 2f,  pieceHeight / 2f, 0),
+                    new Vector3( pieceWidth / 2f,  pieceHeight / 2f, 0)
                 };
                 mesh.vertices = vertices;
                 mesh.triangles = triangles;
@@ -46,41 +139,35 @@ public class PuzzleManager : MonoBehaviour
                 Material material = new Material(Shader.Find("Sprites/Default"));
                 renderer.material = material;
                 material.mainTexture = image;
-                CameraHandler.Instance.FrameObject(piece);
+
+                //randomly places the puzzle pieces on the board
+                int randomSide = UnityEngine.Random.Range(0, 2);
+                switch (randomSide)
+                {
+                    case 0: //left side
+                        piece.transform.position = new Vector3(
+                            UnityEngine.Random.Range(
+                                boardLeft - piecesSpace - piecesPadding - pieceWidth / 2f,
+                                 boardLeft - piecesPadding - pieceWidth / 2f),
+                            UnityEngine.Random.Range(
+                                -boardHeight / 2f,
+                                 boardHeight / 2f),
+                            0
+                        );
+                        break;
+                    case 1: //right side
+                        piece.transform.position = new Vector3(
+                            UnityEngine.Random.Range(
+                                boardRight + piecesPadding + pieceWidth / 2f,
+                                boardRight + piecesSpace + piecesPadding + pieceWidth / 2f),
+                            UnityEngine.Random.Range(
+                                -boardHeight / 2f,
+                                boardHeight / 2f),
+                            0
+                        );
+                        break;
+                }
             }
         }
-
-        /*testing code for here
-        GameObject piece = new GameObject("PuzzlePiece");
-        Mesh mesh = new Mesh();
-        Vector3[] vertices =
-        {
-            new Vector3(-image.width / 2, -image.height / 2, 0),
-            new Vector3(image.width / 2, -image.height / 2, 0),
-            new Vector3(-image.width / 2,  image.height / 2, 0),
-            new Vector3(image.width / 2,  image.height / 2, 0)
-        };
-        mesh.vertices = vertices;
-        int[] triangles =
-        {
-            0, 2, 1,
-            2, 3, 1
-        };
-        mesh.triangles = triangles;
-        Vector2[] uv =
-        {
-            new Vector2(0.0f, 0.0f),
-            new Vector2(0.5f, 0.0f),
-            new Vector2(0.0f, 0.5f),
-            new Vector2(0.5f, 0.5f)
-        };
-        mesh.uv = uv;
-        MeshFilter filter = piece.AddComponent<MeshFilter>();
-        MeshRenderer renderer = piece.AddComponent<MeshRenderer>();
-        filter.mesh = mesh;
-        Material material = new Material(Shader.Find("Sprites/Default"));
-        renderer.material = material;
-        material.mainTexture = image;
-        CameraHandler.Instance.FrameObject(piece);*/
     }
 }
